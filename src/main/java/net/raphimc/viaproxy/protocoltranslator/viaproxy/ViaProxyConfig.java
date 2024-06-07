882aeb93bb1b4004b871ced55b4cc71ef89bfc2d
James <lim.en.hui.james@gmail.com>
2024-06-07 17:42:29 +0100

    Automatically log into realms

--- src/main/java/net/raphimc/viaproxy/protocoltranslator/viaproxy/ViaProxyConfig.java
+++ src/main/java/net/raphimc/viaproxy/protocoltranslator/viaproxy/ViaProxyConfig.java
@@ -17,12 +17,16 @@
  */
 package net.raphimc.viaproxy.protocoltranslator.viaproxy;
 
+import com.google.common.collect.Iterables;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import com.viaversion.viaversion.util.Config;
 import joptsimple.OptionException;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import net.raphimc.minecraftauth.MinecraftAuth;
+import net.raphimc.minecraftauth.service.realms.JavaRealmsService;
+import net.raphimc.minecraftauth.service.realms.model.RealmsWorld;
 import net.raphimc.vialoader.util.JLoggerToSLF4J;
 import net.raphimc.viaproxy.ViaProxy;
 import net.raphimc.viaproxy.cli.BetterHelpFormatter;
@@ -32,6 +36,7 @@ import net.raphimc.viaproxy.plugins.events.PostOptionsParseEvent;
 import net.raphimc.viaproxy.plugins.events.PreOptionsParseEvent;
 import net.raphimc.viaproxy.protocoltranslator.ProtocolTranslator;
 import net.raphimc.viaproxy.saves.impl.accounts.Account;
+import net.raphimc.viaproxy.saves.impl.accounts.MicrosoftAccount;
 import net.raphimc.viaproxy.util.AddressUtil;
 import net.raphimc.viaproxy.util.logging.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,6 +62,7 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
     private final OptionSpec<String> optionTargetAddress;
     private final OptionSpec<ProtocolVersion> optionTargetVersion;
     private final OptionSpec<Boolean> optionProxyOnlineMode;
+    private final OptionSpec<Boolean> optionUseRealms;
     private final OptionSpec<AuthMethod> optionAuthMethod;
     private final OptionSpec<Integer> optionMinecraftAccountIndex;
     private final OptionSpec<Boolean> optionBetacraftAuth;
@@ -78,6 +84,7 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
     private SocketAddress targetAddress = AddressUtil.parse("127.0.0.1:25565", null);
     private ProtocolVersion targetVersion = ProtocolTranslator.AUTO_DETECT_PROTOCOL;
     private boolean proxyOnlineMode = false;
+    private boolean useRealms = false;
     private AuthMethod authMethod = AuthMethod.NONE;
     private Account account = null;
     private boolean betacraftAuth = false;
@@ -100,36 +107,64 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
 
         this.optionParser = new OptionParser();
         this.optionHelp = this.optionParser.accepts("help").forHelp();
-        this.optionBindAddress = this.optionParser.accepts("bind-address").withRequiredArg().ofType(String.class).defaultsTo(AddressUtil.toString(this.bindAddress));
-        this.optionTargetAddress = this.optionParser.accepts("target-address").withRequiredArg().ofType(String.class).defaultsTo(AddressUtil.toString(this.targetAddress));
-        this.optionTargetVersion = this.optionParser.accepts("target-version").withRequiredArg().withValuesConvertedBy(new ProtocolVersionConverter()).defaultsTo(this.targetVersion);
-        this.optionProxyOnlineMode = this.optionParser.accepts("proxy-online-mode").withRequiredArg().ofType(Boolean.class).defaultsTo(this.proxyOnlineMode);
-        this.optionAuthMethod = this.optionParser.accepts("auth-method").withRequiredArg().ofType(AuthMethod.class).defaultsTo(this.authMethod);
-        this.optionMinecraftAccountIndex = this.optionParser.accepts("minecraft-account-index").withRequiredArg().ofType(Integer.class).defaultsTo(0);
-        this.optionBetacraftAuth = this.optionParser.accepts("betacraft-auth").withRequiredArg().ofType(Boolean.class).defaultsTo(this.betacraftAuth);
-        this.optionBackendProxyUrl = this.optionParser.accepts("backend-proxy-url").withRequiredArg().ofType(String.class).defaultsTo("");
-        this.optionBackendHaProxy = this.optionParser.accepts("backend-haproxy").withRequiredArg().ofType(Boolean.class).defaultsTo(this.backendHaProxy);
-        this.optionFrontendHaProxy = this.optionParser.accepts("frontend-haproxy").withRequiredArg().ofType(Boolean.class).defaultsTo(this.frontendHaProxy);
-        this.optionChatSigning = this.optionParser.accepts("chat-signing").withRequiredArg().ofType(Boolean.class).defaultsTo(this.chatSigning);
-        this.optionCompressionThreshold = this.optionParser.accepts("compression-threshold").withRequiredArg().ofType(Integer.class).defaultsTo(this.compressionThreshold);
-        this.optionAllowBetaPinging = this.optionParser.accepts("allow-beta-pinging").withRequiredArg().ofType(Boolean.class).defaultsTo(this.allowBetaPinging);
-        this.optionIgnoreProtocolTranslationErrors = this.optionParser.accepts("ignore-protocol-translation-errors").withRequiredArg().ofType(Boolean.class).defaultsTo(this.ignoreProtocolTranslationErrors);
-        this.optionSuppressClientProtocolErrors = this.optionParser.accepts("suppress-client-protocol-errors").withRequiredArg().ofType(Boolean.class).defaultsTo(this.suppressClientProtocolErrors);
-        this.optionAllowLegacyClientPassthrough = this.optionParser.accepts("allow-legacy-client-passthrough").withRequiredArg().ofType(Boolean.class).defaultsTo(this.allowLegacyClientPassthrough);
-        this.optionCustomMotd = this.optionParser.accepts("custom-motd").withRequiredArg().ofType(String.class).defaultsTo(this.customMotd);
-        this.optionResourcePackUrl = this.optionParser.accepts("resource-pack-url").withRequiredArg().ofType(String.class).defaultsTo(this.resourcePackUrl);
-        this.optionWildcardDomainHandling = this.optionParser.accepts("wildcard-domain-handling").withRequiredArg().ofType(WildcardDomainHandling.class).defaultsTo(this.wildcardDomainHandling);
-        this.optionSimpleVoiceChatSupport = this.optionParser.accepts("simple-voice-chat-support").withRequiredArg().ofType(Boolean.class).defaultsTo(this.simpleVoiceChatSupport);
+        this.optionBindAddress = this.optionParser.accepts("bind-address").withRequiredArg().ofType(String.class)
+                .defaultsTo(AddressUtil.toString(this.bindAddress));
+        this.optionTargetAddress = this.optionParser.accepts("target-address").withRequiredArg().ofType(String.class)
+                .defaultsTo(AddressUtil.toString(this.targetAddress));
+        this.optionTargetVersion = this.optionParser.accepts("target-version").withRequiredArg()
+                .withValuesConvertedBy(new ProtocolVersionConverter()).defaultsTo(this.targetVersion);
+        this.optionProxyOnlineMode = this.optionParser.accepts("proxy-online-mode").withRequiredArg()
+                .ofType(Boolean.class).defaultsTo(this.proxyOnlineMode);
+        this.optionUseRealms = this.optionParser.accepts("use-realms").withRequiredArg()
+                .ofType(Boolean.class).defaultsTo(false);
+        this.optionAuthMethod = this.optionParser.accepts("auth-method").withRequiredArg().ofType(AuthMethod.class)
+                .defaultsTo(this.authMethod);
+        this.optionMinecraftAccountIndex = this.optionParser.accepts("minecraft-account-index").withRequiredArg()
+                .ofType(Integer.class).defaultsTo(0);
+        this.optionBetacraftAuth = this.optionParser.accepts("betacraft-auth").withRequiredArg().ofType(Boolean.class)
+                .defaultsTo(this.betacraftAuth);
+        this.optionBackendProxyUrl = this.optionParser.accepts("backend-proxy-url").withRequiredArg()
+                .ofType(String.class).defaultsTo("");
+        this.optionBackendHaProxy = this.optionParser.accepts("backend-haproxy").withRequiredArg().ofType(Boolean.class)
+                .defaultsTo(this.backendHaProxy);
+        this.optionFrontendHaProxy = this.optionParser.accepts("frontend-haproxy").withRequiredArg()
+                .ofType(Boolean.class).defaultsTo(this.frontendHaProxy);
+        this.optionChatSigning = this.optionParser.accepts("chat-signing").withRequiredArg().ofType(Boolean.class)
+                .defaultsTo(this.chatSigning);
+        this.optionCompressionThreshold = this.optionParser.accepts("compression-threshold").withRequiredArg()
+                .ofType(Integer.class).defaultsTo(this.compressionThreshold);
+        this.optionAllowBetaPinging = this.optionParser.accepts("allow-beta-pinging").withRequiredArg()
+                .ofType(Boolean.class).defaultsTo(this.allowBetaPinging);
+        this.optionIgnoreProtocolTranslationErrors = this.optionParser.accepts("ignore-protocol-translation-errors")
+                .withRequiredArg().ofType(Boolean.class).defaultsTo(this.ignoreProtocolTranslationErrors);
+        this.optionSuppressClientProtocolErrors = this.optionParser.accepts("suppress-client-protocol-errors")
+                .withRequiredArg().ofType(Boolean.class).defaultsTo(this.suppressClientProtocolErrors);
+        this.optionAllowLegacyClientPassthrough = this.optionParser.accepts("allow-legacy-client-passthrough")
+                .withRequiredArg().ofType(Boolean.class).defaultsTo(this.allowLegacyClientPassthrough);
+        this.optionCustomMotd = this.optionParser.accepts("custom-motd").withRequiredArg().ofType(String.class)
+                .defaultsTo(this.customMotd);
+        this.optionResourcePackUrl = this.optionParser.accepts("resource-pack-url").withRequiredArg()
+                .ofType(String.class).defaultsTo(this.resourcePackUrl);
+        this.optionWildcardDomainHandling = this.optionParser.accepts("wildcard-domain-handling").withRequiredArg()
+                .ofType(WildcardDomainHandling.class).defaultsTo(this.wildcardDomainHandling);
+        this.optionSimpleVoiceChatSupport = this.optionParser.accepts("simple-voice-chat-support").withRequiredArg()
+                .ofType(Boolean.class).defaultsTo(this.simpleVoiceChatSupport);
     }
 
     @Override
     public void reload() {
         super.reload();
 
-        this.bindAddress = AddressUtil.parse(this.getString("bind-address", AddressUtil.toString(this.bindAddress)), null);
-        this.targetVersion = ProtocolVersion.getClosest(this.getString("target-version", this.targetVersion.getName()));
-        this.checkTargetVersion();
-        this.targetAddress = AddressUtil.parse(this.getString("target-address", AddressUtil.toString(this.targetAddress)), this.targetVersion);
+        this.bindAddress = AddressUtil.parse(this.getString("bind-address", AddressUtil.toString(this.bindAddress)),
+                null);
+        this.useRealms = this.getBoolean("use-realms", this.useRealms);
+        this.targetVersion = ProtocolVersion
+                .getClosest(this.getString("target-version", this.targetVersion.getName()));
+        if (!this.useRealms) {
+            this.targetAddress = AddressUtil
+                    .parse(this.getString("target-address", AddressUtil.toString(this.targetAddress)),
+                            this.targetVersion);
+        }
         this.proxyOnlineMode = this.getBoolean("proxy-online-mode", this.proxyOnlineMode);
         this.authMethod = AuthMethod.byName(this.getString("auth-method", this.authMethod.name()));
         final List<Account> accounts = ViaProxy.getSaveManager().accountsSave.getAccounts();
@@ -139,6 +174,47 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
         } else {
             this.account = null;
         }
+
+        if (this.useRealms) {
+            this.logger.info("Recognized useRealms");
+            if (this.account == null) {
+                this.logger.severe("No account to select realms");
+            }
+
+            if (!(account instanceof MicrosoftAccount)) {
+                this.logger.severe("Cannot connect to Realms with a non-microsoft account");
+            }
+
+            final JavaRealmsService realmsService = new JavaRealmsService(MinecraftAuth.createHttpClient(),
+                    Iterables.getLast(this.targetVersion.getIncludedVersions()),
+                    ((MicrosoftAccount) account).getMcProfile());
+
+            realmsService.isAvailable().thenAccept(state -> {
+                if (state) {
+                    realmsService.getWorlds().thenAccept(worlds -> {
+                        final String joinWithUser = this.getString("target-address", "");
+                        this.logger.info("Will join " + joinWithUser);
+
+                        for (RealmsWorld world : worlds) {
+                            if (!world.getOwnerName().equals(joinWithUser)) {
+                                this.logger.info("Skipping world owned by " + world.getOwnerName());
+                                continue;
+                            }
+                            this.logger.info("Found world owned by " + world.getOwnerName());
+
+                            realmsService.joinWorld(world).thenAccept(address -> {
+                                this.targetAddress = AddressUtil
+                                        .parse(address, this.targetVersion);
+                            }).join();
+                            break;
+                        }
+                    }).join();
+                } else {
+                    this.logger.severe("Realms have no state");
+                }
+            }).join();
+        }
+        this.checkTargetVersion();
         this.betacraftAuth = this.getBoolean("betacraft-auth", this.betacraftAuth);
         this.backendProxyUrl = this.parseProxyUrl(this.getString("backend-proxy-url", ""));
         this.backendHaProxy = this.getBoolean("backend-haproxy", this.backendHaProxy);
@@ -146,12 +222,16 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
         this.chatSigning = this.getBoolean("chat-signing", this.chatSigning);
         this.compressionThreshold = this.getInt("compression-threshold", this.compressionThreshold);
         this.allowBetaPinging = this.getBoolean("allow-beta-pinging", this.allowBetaPinging);
-        this.ignoreProtocolTranslationErrors = this.getBoolean("ignore-protocol-translation-errors", this.ignoreProtocolTranslationErrors);
-        this.suppressClientProtocolErrors = this.getBoolean("suppress-client-protocol-errors", this.suppressClientProtocolErrors);
-        this.allowLegacyClientPassthrough = this.getBoolean("allow-legacy-client-passthrough", this.allowLegacyClientPassthrough);
+        this.ignoreProtocolTranslationErrors = this.getBoolean("ignore-protocol-translation-errors",
+                this.ignoreProtocolTranslationErrors);
+        this.suppressClientProtocolErrors = this.getBoolean("suppress-client-protocol-errors",
+                this.suppressClientProtocolErrors);
+        this.allowLegacyClientPassthrough = this.getBoolean("allow-legacy-client-passthrough",
+                this.allowLegacyClientPassthrough);
         this.customMotd = this.getString("custom-motd", this.customMotd);
         this.resourcePackUrl = this.getString("resource-pack-url", this.resourcePackUrl);
-        this.wildcardDomainHandling = WildcardDomainHandling.byName(this.getString("wildcard-domain-handling", this.wildcardDomainHandling.name()));
+        this.wildcardDomainHandling = WildcardDomainHandling
+                .byName(this.getString("wildcard-domain-handling", this.wildcardDomainHandling.name()));
         this.simpleVoiceChatSupport = this.getBoolean("simple-voice-chat-support", this.simpleVoiceChatSupport);
     }
 
@@ -409,7 +489,8 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
     private void checkTargetVersion() {
         if (this.targetVersion == null) {
             this.targetVersion = ProtocolTranslator.AUTO_DETECT_PROTOCOL;
-            this.logger.info("Invalid target version: " + this.getString("target-version", "") + ". Defaulting to auto detect.");
+            this.logger.info(
+                    "Invalid target version: " + this.getString("target-version", "") + ". Defaulting to auto detect.");
             this.logger.info("=== Supported Protocol Versions ===");
             for (ProtocolVersion version : ProtocolVersion.getProtocols()) {
                 this.logger.info(version.getName());
@@ -423,7 +504,8 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
             try {
                 return new URI(proxyUrl);
             } catch (URISyntaxException e) {
-                throw new IllegalArgumentException("Invalid proxy url: " + proxyUrl + ". Proxy url format: type://address:port or type://username:password@address:port");
+                throw new IllegalArgumentException("Invalid proxy url: " + proxyUrl
+                        + ". Proxy url format: type://address:port or type://username:password@address:port");
             }
         }
 
@@ -433,7 +515,8 @@ public class ViaProxyConfig extends Config implements com.viaversion.viaversion.
     public enum AuthMethod {
 
         /**
-         * Use an account for joining the target server (Has to be configured in ViaProxy GUI)
+         * Use an account for joining the target server (Has to be configured in
+         * ViaProxy GUI)
          */
         ACCOUNT("tab.general.minecraft_account.option_select_account"),
         /**
